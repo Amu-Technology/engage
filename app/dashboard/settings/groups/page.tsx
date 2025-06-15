167ceefee978a4a92a6d7b4e7fb2cf2ec2d19c94
DESKTOP-LAO0HPD\flowe <fhaimai2024@gmail.com>
2025-06-15 10:01:52 +0900

    グループ設定画面の修正 [#7]

--- app/dashboard/settings/groups/page.tsx
+++ app/dashboard/settings/groups/page.tsx
@@ -7,15 +7,11 @@ import { GroupManager } from './components/GroupManager'
 export default function GroupsSettingsPage() {
   return (
     <div className="container mx-auto p-4 space-y-4">
-      <h1 className="text-2xl font-bold">設定</h1>
       <Tabs defaultValue="groups">
-        <TabsList>
-          <TabsTrigger value="groups">グループ</TabsTrigger>
-        </TabsList>
         <TabsContent value="groups">
           <Card>
             <CardHeader>
-              <CardTitle>グループの管理</CardTitle>
+              <CardTitle>グループの作成</CardTitle>
             </CardHeader>
             <CardContent>
               <GroupManager />
